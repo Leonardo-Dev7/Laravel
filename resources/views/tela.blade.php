376028cd7193056097f9f36c70d17f34ebe9bfc9
Leonardo Silva <leonardosilvapinto142@gmail.com>
2026-03-18 11:11:52 -0300

    template simples de html em laravel

---
+++ resources/views/tela.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Primeira Tela no Laravel</title>
+    
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+
+    <div class="cartao">
+        <h1>Olá, Laravel!</h1>
+        <p>Esta é uma tela simples feita em HTML e estilizada com CSS nativo dentro da estrutura do Laravel.</p>
+        <a href="#" class="botao">Saiba Mais</a>
+    </div>
+
+</body>
+</html>
