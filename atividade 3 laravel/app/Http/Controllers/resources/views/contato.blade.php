34a5cdb3152a9088610ec0e06db766258a13113a
Leonardo Silva <leonardosilvapinto142@gmail.com>
2026-03-18 15:58:23 -0300

    Delete atividade 3 laravel/app/Http/Controllers directory

--- atividade 3 laravel/app/Http/Controllers/resources/views/contato.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout')
-
-@section('conteudo')
-    <h1>Fale Conosco</h1>
-    <p>Envie um e-mail para contato@meusite.com</p>
-@endsection
--- atividade 3 laravel/app/Http/Controllers/resources/views/home.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout')
-
-@section('conteudo')
-    <h1>Bem-vindo à nossa página inicial!</h1>
-    <p>Este é o conteúdo exclusivo da Home.</p>
-@endsection
